4968159d5e340d680ed904274b0e2b12537518e3
Seth T.  Traman <celltheman123@gmail.com>
2016-10-19 17:45:54 +0000

    Published from Cloud 9

--- public/js/build/latest.js
+++ public/js/build/latest.js
@@ -1,6 +1,4 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-"use strict";
-
 /* global THREE, CANNON */
 
 module.exports = {
@@ -21,7 +19,7 @@ module.exports = {
         projectiles: []
     },
     LABELS: [],
-
+    
     otherPlayers: [],
     otherPlayersId: [],
 
@@ -31,14 +29,11 @@ module.exports = {
 };
 
 module.exports.rendererDEBUG = new THREE.CannonDebugRenderer(module.exports.scene, module.exports.world);
-
 },{}],2:[function(require,module,exports){
-"use strict";
-
 /* global THREE, CANNON */
 
 module.exports = function (globals) {
-
+    
     var groundShape = new CANNON.Plane();
     var groundBody = new CANNON.Body({
         mass: 0
@@ -75,16 +70,13 @@ module.exports = function (globals) {
 
     var mesh = new THREE.Mesh(geometry, new THREE.MeshLambertMaterial({
         color: 0x00FF00,
-        shininess: 10
+        shininess: 10,
     }));
     mesh.castShadow = true;
     mesh.receiveShadow = true;
     globals.scene.add(mesh);
 };
-
 },{}],3:[function(require,module,exports){
-'use strict';
-
 function init(globals) {
 
     require('./world')(globals);
@@ -96,18 +88,16 @@ function init(globals) {
     globals.renderer.setClearColor(globals.scene.fog.color, 1);
     globals.renderer.setSize(window.innerWidth, window.innerHeight);
 
-    document.body.appendChild(globals.renderer.domElement);
+    document.body.appendChild( globals.renderer.domElement );
+
 }
 
 module.exports = init;
-
 },{"./bodies":2,"./player":4,"./world":5}],4:[function(require,module,exports){
-'use strict';
-
 /* global THREE, CANNON, PointerLockControls */
 
 module.exports = function (globals) {
-
+    
     var mass = 5,
         radius = 1.3;
     var sphereShape = new CANNON.Sphere(radius);
@@ -124,19 +114,20 @@ module.exports = function (globals) {
         shape: sphereShape
     };
 
-    globals.MESHES['player'] = new THREE.Mesh(new THREE.BoxGeometry(1, 2.5, 1), new THREE.MeshLambertMaterial());
+    globals.MESHES['player'] = new THREE.Mesh(
+        new THREE.BoxGeometry(1, 2.5, 1),
+        new THREE.MeshLambertMaterial()
+    );
     globals.MESHES['player'].castShadow = true;
     globals.scene.add(globals.MESHES['player']);
 
     globals.controls = new PointerLockControls(globals.camera, globals.BODIES['player'].body);
     globals.scene.add(globals.controls.getObject());
-
+    
     window.controls = globals.controls;
+    
 };
-
 },{}],5:[function(require,module,exports){
-"use strict";
-
 /* global CANNON */
 
 module.exports = function (globals) {
@@ -150,16 +141,17 @@ module.exports = function (globals) {
     solver.iterations = 7;
     solver.tolerance = 0.1;
     var split = true;
-    if (split) globals.world.solver = new CANNON.SplitSolver(solver);else globals.world.solver = solver;
+    if (split)
+        globals.world.solver = new CANNON.SplitSolver(solver);
+    else
+        globals.world.solver = solver;
 
     globals.world.gravity.set(0, -20, 0);
     globals.world.broadphase = new CANNON.NaiveBroadphase();
+    
 };
-
 },{}],6:[function(require,module,exports){
-'use strict';
-
-module.exports = function (globals) {
+module.exports = (globals) => {
 
     function load(mesh, opts) {
         opts = opts ? opts : {};
@@ -265,9 +257,10 @@ module.exports = function (globals) {
         globals.LABELS.push(function () {
             var position = THREEx.ObjCoord.cssPosition(mesh, globals.camera, globals.renderer);
             var boundingRect = element.getBoundingClientRect();
-            element.style.left = position.x - boundingRect.width / 2 + 'px';
-            element.style.top = position.y - boundingRect.height / 2 - 70 + 'px';
-            if (globals.frustum.intersectsObject(mesh)) element.style.opacity = 1;else element.style.opacity = 0;
+            element.style.left = (position.x - boundingRect.width / 2) + 'px';
+            element.style.top = (position.y - boundingRect.height / 2 - 70) + 'px';
+            if (globals.frustum.intersectsObject(mesh)) element.style.opacity = 1;
+            else element.style.opacity = 0;
         });
     }
 
@@ -281,16 +274,13 @@ module.exports = function (globals) {
     // this.label = label;
     // this.ball = ball;
 };
-
 },{}],7:[function(require,module,exports){
-'use strict';
-
 /* global socket */
 
-var globals = require('./globals');
-var player = require('./player');
+let globals = require('./globals');
+let player = require('./player');
 
-var dt = 1 / 60;
+const dt = 1 / 60;
 
 require('./init/manager')(globals);
 require('./load')(globals);
@@ -309,21 +299,21 @@ function animate(delta) {
     globals.frustum.setFromMatrix(globals.cameraViewProjectionMatrix);
 
     // Update bullets, etc.
-    for (var i = 0; i < globals.BODIES['projectiles'].length; i++) {
+    for (let i = 0; i < globals.BODIES['projectiles'].length; i++) {
         globals.BODIES['projectiles'][i].mesh.position.copy(globals.BODIES['projectiles'][i].body.position);
         globals.BODIES['projectiles'][i].mesh.quaternion.copy(globals.BODIES['projectiles'][i].body.quaternion);
     }
 
     // Update items
-    for (var _i = 0; _i < globals.BODIES['items'].length; _i++) {
-        globals.BODIES['items'][_i].mesh.position.copy(globals.BODIES['items'][_i].body.position);
-        globals.BODIES['items'][_i].mesh.quaternion.copy(globals.BODIES['items'][_i].body.quaternion);
+    for (let i = 0; i < globals.BODIES['items'].length; i++) {
+        globals.BODIES['items'][i].mesh.position.copy(globals.BODIES['items'][i].body.position);
+        globals.BODIES['items'][i].mesh.quaternion.copy(globals.BODIES['items'][i].body.quaternion);
     }
 
     // Update labels
-    for (var key in globals.LABELS) {
-        globals.LABELS[key]();
-    }globals.BODIES['player'].body.velocity.set(globals.BODIES['player'].body.velocity.x * 0.95, globals.BODIES['player'].body.velocity.y, globals.BODIES['player'].body.velocity.z * 0.95);
+    for (let key in globals.LABELS) globals.LABELS[key]();
+
+    globals.BODIES['player'].body.velocity.set(globals.BODIES['player'].body.velocity.x * 0.95, globals.BODIES['player'].body.velocity.y, globals.BODIES['player'].body.velocity.z * 0.95)
     globals.MESHES['player'].position.copy(globals.BODIES['player'].body.position);
 
     globals.world.step(dt);
@@ -336,30 +326,29 @@ function animate(delta) {
         globals.updatePlayerData();
         socket.emit('updatePosition', player.serverdata);
     }
+
 }
-
 },{"./globals":1,"./init/manager":3,"./load":6,"./multiplayer":8,"./player":9,"./shooting":10}],8:[function(require,module,exports){
-'use strict';
-
 /* global socket */
 
-module.exports = function (globals, player) {
+module.exports = (globals, player) => {
 
     socket.emit('requestOldPlayers', {});
 
-    socket.on('createPlayer', function (data) {
+    socket.on('createPlayer', data => {
         if (typeof player.serverdata === 'undefined') {
 
             player.serverdata = data;
             player.id = data.id;
 
             player.inventory = player.serverdata.acc.inventory;
+
         }
     });
 
-    socket.on('addOtherPlayer', function (data) {
+    socket.on('addOtherPlayer', data => {
         if (data.id !== player.id) {
-            var cube = globals.load.box({
+            let cube = globals.load.box({
                 l: 1,
                 w: 1,
                 h: 2,
@@ -372,16 +361,17 @@ module.exports = function (globals, player) {
         }
     });
 
-    socket.on('removeOtherPlayer', function (data) {
+    socket.on('removeOtherPlayer', data => {
 
         globals.scene.remove(playerForId(data.id).mesh);
         globals.world.remove(playerForId(data.id).body);
         console.log(data.id + ' disconnected');
+
     });
 
-    socket.on('updatePosition', function (data) {
+    socket.on('updatePosition', data => {
 
-        var somePlayer = playerForId(data.id);
+        let somePlayer = playerForId(data.id);
         if (somePlayer) {
             somePlayer.body.position.x = data.x;
             somePlayer.body.position.y = data.y;
@@ -392,29 +382,32 @@ module.exports = function (globals, player) {
             // somePlayer.body.q.z = data.q.z;
             // somePlayer.body.q.w = data.q.w;
         }
+
     });
 
-    socket.on('bullet', function (pos) {
+    socket.on('bullet', (pos) => {
         alert('boi');
-        var ball = globals.load.ball({
+        let ball = globals.load.ball({
             array: 'projectiles'
         });
         ball.body.position.set(pos.x, pos.y, pos.z);
         ball.mesh.position.set(pos.x, pos.y, pos.z);
     });
 
-    var updatePlayerData = function updatePlayerData() {
 
+    var updatePlayerData = function () {
+        
         player.serverdata.x = globals.BODIES['player'].body.position.x;
         player.serverdata.y = globals.BODIES['player'].body.position.y;
         player.serverdata.z = globals.BODIES['player'].body.position.z;
 
         player.serverdata.q = globals.BODIES['player'].body.quaternion;
+        
     };
 
-    var playerForId = function playerForId(id) {
-        var index = void 0;
-        for (var i = 0; i < globals.otherPlayersId.length; i++) {
+    var playerForId = id => {
+        let index;
+        for (let i = 0; i < globals.otherPlayersId.length; i++) {
             if (globals.otherPlayersId[i] == id) {
                 index = i;
                 break;
@@ -435,45 +428,42 @@ module.exports = function (globals, player) {
 
     globals.updatePlayerData = updatePlayerData;
     globals.playerForId = playerForId;
+
 };
-
 },{}],9:[function(require,module,exports){
-"use strict";
-
 module.exports = {};
-
 },{}],10:[function(require,module,exports){
-'use strict';
-
 /* global THREE, CANNON, socket */
 
-module.exports = function (globals) {
+module.exports = (globals) => {
 
     function getShootDir(targetVec) {
-        var projector = new THREE.Projector();
-        var vector = targetVec;
+        let projector = new THREE.Projector();
+        let vector = targetVec;
         targetVec.set(0, 0, 1);
         projector.unprojectVector(vector, globals.camera);
-        var ray = new THREE.Ray(globals.BODIES['player'].body.position, vector.sub(globals.BODIES['player'].body.position).normalize());
+        let ray = new THREE.Ray(globals.BODIES['player'].body.position, vector.sub(globals.BODIES['player'].body.position).normalize());
         targetVec.copy(ray.direction);
     }
 
-    window.addEventListener("click", function (e) {
+    window.addEventListener("click", (e) => {
         if (globals.controls.enabled == true) {
 
-            var shootDirection = new THREE.Vector3();
-            var shootVelo = 15;
+            let shootDirection = new THREE.Vector3();
+            const shootVelo = 15;
 
-            var x = globals.BODIES['player'].body.position.x;
-            var y = globals.BODIES['player'].body.position.y;
-            var z = globals.BODIES['player'].body.position.z;
+            let x = globals.BODIES['player'].body.position.x;
+            let y = globals.BODIES['player'].body.position.y;
+            let z = globals.BODIES['player'].body.position.z;
 
-            var ball = globals.load.ball({
+            let ball = globals.load.ball({
                 array: 'projectiles'
             });
 
             getShootDir(shootDirection);
-            ball.body.velocity.set(shootDirection.x * shootVelo, shootDirection.y * shootVelo, shootDirection.z * shootVelo);
+            ball.body.velocity.set(shootDirection.x * shootVelo,
+                shootDirection.y * shootVelo,
+                shootDirection.z * shootVelo);
 
             // Move the ball outside the player sphere
             x += shootDirection.x * (globals.BODIES['player'].shape.radius * 1.02 + ball.shape.radius);
@@ -481,14 +471,13 @@ module.exports = function (globals) {
             z += shootDirection.z * (globals.BODIES['player'].shape.radius * 1.02 + ball.shape.radius);
             ball.body.position.set(x, y, z);
             ball.mesh.position.set(x, y, z);
-
+            
             socket.emit('bullet', {
-                x: x,
-                y: y,
-                z: z
+                x,
+                y,
+                z
             });
         }
     });
 };
-
-},{}]},{},[1,2,3,4,5,6,7,8,9,10]);
+},{}]},{},[7]);
