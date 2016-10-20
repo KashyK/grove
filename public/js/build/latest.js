92ce01eddafd62175f1f1016507f17fb810aaf95
Seth T.  Traman <celltheman123@gmail.com>
2016-10-20 12:54:47 +0000

    Published from Cloud 9

--- public/js/build/latest.js
+++ public/js/build/latest.js
@@ -92,7 +92,7 @@ module.exports = function (globals) {
 /* global $ */
 
 module.exports = function () {
-    $('#splash-title').text('In the beginning, there was the grove tree...');
+    $('#splash-title').text('In the beginning, a Tree was planted...');
     setTimeout(function () {
 
         $('#splash-title').fadeOut(2000);
@@ -113,7 +113,7 @@ module.exports = function () {
 
                 $('#splash-title').fadeOut(2000);
                 setTimeout(function () {
-                    $('#splash-title').text('These tried to create a power of their own to combat those naturally infused...');
+                    $('#splash-title').text('12 Alchemists, of heightened skill...');
                 }, 2000);
                 $('#splash-title').fadeIn(2000);
 
@@ -121,7 +121,7 @@ module.exports = function () {
 
                     $('#splash-title').fadeOut(2000);
                     setTimeout(function () {
-                        $('#splash-title').text('Eventually, they succeeded, but then...');
+                        $('#splash-title').text('They attempted to create a power of their own, to combat those naturally infused...');
                     }, 2000);
                     $('#splash-title').fadeIn(2000);
 
@@ -129,9 +129,54 @@ module.exports = function () {
 
                         $('#splash-title').fadeOut(2000);
                         setTimeout(function () {
-                            $('#splash-title').text('Their creation consumed them, turning them into unimaginable demons...');
+                            $('#splash-title').text('Eventually, they succeeded, but...');
                         }, 2000);
                         $('#splash-title').fadeIn(2000);
+
+                        setTimeout(function () {
+
+                            $('#splash-title').fadeOut(2000);
+                            setTimeout(function () {
+                                $('#splash-title').text('Their creation consumed them, turning them into unimaginable demons...');
+                            }, 2000);
+                            $('#splash-title').fadeIn(2000);
+
+                            setTimeout(function () {
+
+                                $('#splash-title').fadeOut(2000);
+                                setTimeout(function () {
+                                    $('#splash-title').text('Finally, put to rest by The Developer, they were banished from these lands...');
+                                }, 2000);
+                                $('#splash-title').fadeIn(2000);
+
+                                setTimeout(function () {
+
+                                    $('#splash-title').fadeOut(2000);
+                                    setTimeout(function () {
+                                        $('#splash-title').text('Now, they are back, attacking the original Tree and its power...');
+                                    }, 2000);
+                                    $('#splash-title').fadeIn(2000);
+
+                                    setTimeout(function () {
+
+                                        $('#splash-title').fadeOut(2000);
+                                        setTimeout(function () {
+                                            $('#splash-title').text('And it is up to you to stop them.');
+                                        }, 2000);
+                                        $('#splash-title').fadeIn(2000);
+
+                                        setTimeout(function () {
+
+                                            $('#splash-title').fadeOut(2000);
+                                            setTimeout(function () {
+                                                $('#splash-title').text('THE GROVE');
+                                            }, 2000);
+                                            $('#splash-title').fadeIn(2000);
+                                        }, 6500);
+                                    }, 6500);
+                                }, 6500);
+                            }, 6500);
+                        }, 6500);
                     }, 6500);
                 }, 6500);
             }, 6500);
--- src/init/dom.js
+++ src/init/dom.js
@@ -1,7 +1,7 @@
 /* global $ */
 
 module.exports = () => {
-    $('#splash-title').text('In the beginning, there was the grove tree...');
+    $('#splash-title').text('In the beginning, a Tree was planted...');
     setTimeout(function () {
 
         $('#splash-title').fadeOut(2000);
@@ -22,7 +22,7 @@ module.exports = () => {
 
                 $('#splash-title').fadeOut(2000);
                 setTimeout(function () {
-                    $('#splash-title').text('These tried to create a power of their own to combat those naturally infused...');
+                    $('#splash-title').text('12 Alchemists, of heightened skill...');
                 }, 2000);
                 $('#splash-title').fadeIn(2000);
 
@@ -30,7 +30,7 @@ module.exports = () => {
 
                     $('#splash-title').fadeOut(2000);
                     setTimeout(function () {
-                        $('#splash-title').text('Eventually, they succeeded, but then...');
+                        $('#splash-title').text('They attempted to create a power of their own, to combat those naturally infused...');
                     }, 2000);
                     $('#splash-title').fadeIn(2000);
 
@@ -38,9 +38,59 @@ module.exports = () => {
 
                         $('#splash-title').fadeOut(2000);
                         setTimeout(function () {
-                            $('#splash-title').text('Their creation consumed them, turning them into unimaginable demons...');
+                            $('#splash-title').text('Eventually, they succeeded, but...');
                         }, 2000);
                         $('#splash-title').fadeIn(2000);
+
+                        setTimeout(function () {
+
+                            $('#splash-title').fadeOut(2000);
+                            setTimeout(function () {
+                                $('#splash-title').text('Their creation consumed them, turning them into unimaginable demons...');
+                            }, 2000);
+                            $('#splash-title').fadeIn(2000);
+
+                            setTimeout(function () {
+
+                                $('#splash-title').fadeOut(2000);
+                                setTimeout(function () {
+                                    $('#splash-title').text('Finally, put to rest by The Developer, they were banished from these lands...');
+                                }, 2000);
+                                $('#splash-title').fadeIn(2000);
+
+                                setTimeout(function () {
+
+                                    $('#splash-title').fadeOut(2000);
+                                    setTimeout(function () {
+                                        $('#splash-title').text('Now, they are back, attacking the original Tree and its power...');
+                                    }, 2000);
+                                    $('#splash-title').fadeIn(2000);
+
+                                    setTimeout(function () {
+
+                                        $('#splash-title').fadeOut(2000);
+                                        setTimeout(function () {
+                                            $('#splash-title').text('And it is up to you to stop them.');
+                                        }, 2000);
+                                        $('#splash-title').fadeIn(2000);
+
+                                        setTimeout(function () {
+
+                                            $('#splash-title').fadeOut(2000);
+                                            setTimeout(function () {
+                                                $('#splash-title').text('THE GROVE');
+                                            }, 2000);
+                                            $('#splash-title').fadeIn(2000);
+
+                                        }, 6500);
+
+                                    }, 6500);
+
+                                }, 6500);
+
+                            }, 6500);
+
+                        }, 6500);
 
                     }, 6500);
 
