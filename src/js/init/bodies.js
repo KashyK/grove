ce8c3886d61141aa72d1a6d245a8f40ae546570a
Seth T.  Traman <celltheman123@gmail.com>
2017-01-13 17:34:39 +0000

    Pushed from C9

--- src/js/init/bodies.js
+++ src/js/init/bodies.js
@@ -120,6 +120,13 @@ module.exports = (globals, player) => {
     globals.BODIES['player'].mesh.add(sky);
 
     let loader = new THREE.ObjectLoader();
+    loader.load('/models/herbert/super-magic-dude.json', object => {
+        globals.scene.add(object);
+        object.position.set(0, 0, 0);
+        object.scale.set(0.1, 0.1, 0.1);
+    });
+
+    let loader = new THREE.ObjectLoader();
     loader.load(`/models/sand/sand.json`, object => {
         globals.scene.add(object);
         object.castShadow = true;
